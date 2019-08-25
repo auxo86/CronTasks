ee856474b223acd1acca00d1d14e752b1dc383fd
auxo <auxo_86@hotmail.com>
2019-08-25 23:35:06 +0800

    移除不必要的using

--- CronTasks/Program.cs
+++ CronTasks/Program.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Runtime.CompilerServices;
 using System.Timers;
-using System.Threading.Tasks;
 
 
 namespace CronTasks
